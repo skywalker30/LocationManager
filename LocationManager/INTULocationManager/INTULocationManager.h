a57dead2658a4f9fac0cfaecb5355063d35127df
Roman <rsukner@gmail.com>
2017-05-28 10:43:20 +0300

    Indicates that locationRequests array is empty therefore the location manager is idle

--- LocationManager/INTULocationManager/INTULocationManager.h
+++ LocationManager/INTULocationManager/INTULocationManager.h
@@ -55,8 +55,8 @@ __INTU_ASSUME_NONNULL_BEGIN
  Asynchronously requests the current location of the device using location services.
  
  @param desiredAccuracy The accuracy level desired (refers to the accuracy and recency of the location).
- @param timeout         The maximum amount of time (in seconds) to wait for a location with the desired accuracy before completing. If 
-                        this value is 0.0, no timeout will be set (will wait indefinitely for success, unless request is force completed or canceled).
+ @param timeout         The maximum amount of time (in seconds) to wait for a location with the desired accuracy before completing. If
+ this value is 0.0, no timeout will be set (will wait indefinitely for success, unless request is force completed or canceled).
  @param block           The block to execute upon success, failure, or timeout.
  
  @return The location request ID, which can be used to force early completion or cancel the request while it is in progress.
@@ -71,10 +71,10 @@ __INTU_ASSUME_NONNULL_BEGIN
  
  @param desiredAccuracy      The accuracy level desired (refers to the accuracy and recency of the location).
  @param timeout              The maximum amount of time (in seconds) to wait for a location with the desired accuracy before completing. If
-                             this value is 0.0, no timeout will be set (will wait indefinitely for success, unless request is force completed or canceled).
+ this value is 0.0, no timeout will be set (will wait indefinitely for success, unless request is force completed or canceled).
  @param delayUntilAuthorized A flag specifying whether the timeout should only take effect after the user responds to the system prompt requesting
-                             permission for this app to access location services. If YES, the timeout countdown will not begin until after the
-                             app receives location services permissions. If NO, the timeout countdown begins immediately when calling this method.
+ permission for this app to access location services. If YES, the timeout countdown will not begin until after the
+ app receives location services permissions. If NO, the timeout countdown begins immediately when calling this method.
  @param block                The block to execute upon success, failure, or timeout.
  
  @return The location request ID, which can be used to force early completion or cancel the request while it is in progress.
@@ -89,8 +89,8 @@ __INTU_ASSUME_NONNULL_BEGIN
  This method instructs location services to use the highest accuracy available (which also requires the most power).
  If an error occurs, the block will execute with a status other than INTULocationStatusSuccess, and the subscription will be canceled automatically.
  
- @param block The block to execute every time an updated location is available. 
-              The status will be INTULocationStatusSuccess unless an error occurred; it will never be INTULocationStatusTimedOut.
+ @param block The block to execute every time an updated location is available.
+ The status will be INTULocationStatusSuccess unless an error occurred; it will never be INTULocationStatusTimedOut.
  
  @return The location request ID, which can be used to cancel the subscription of location updates to this block.
  */
@@ -103,8 +103,8 @@ __INTU_ASSUME_NONNULL_BEGIN
  
  @param desiredAccuracy The accuracy level desired, which controls how much power is used by the device's location services.
  @param block           The block to execute every time an updated location is available. Note that this block runs for every update, regardless of
-                        whether the achievedAccuracy is at least the desiredAccuracy.
-                        The status will be INTULocationStatusSuccess unless an error occurred; it will never be INTULocationStatusTimedOut.
+ whether the achievedAccuracy is at least the desiredAccuracy.
+ The status will be INTULocationStatusSuccess unless an error occurred; it will never be INTULocationStatusTimedOut.
  
  @return The location request ID, which can be used to cancel the subscription of location updates to this block.
  */
@@ -116,15 +116,15 @@ __INTU_ASSUME_NONNULL_BEGIN
  If an error occurs, the block will execute with a status other than INTULocationStatusSuccess, and the subscription will be canceled automatically.
  
  @param block The block to execute every time an updated location is available.
-              The status will be INTULocationStatusSuccess unless an error occurred; it will never be INTULocationStatusTimedOut.
+ The status will be INTULocationStatusSuccess unless an error occurred; it will never be INTULocationStatusTimedOut.
  
  @return The location request ID, which can be used to cancel the subscription of significant location changes to this block.
  */
 - (INTULocationRequestID)subscribeToSignificantLocationChangesWithBlock:(INTULocationRequestBlock)block;
 
 /** Immediately forces completion of the location request with the given requestID (if it exists), and executes the original request block with the results.
-    For one-time location requests, this is effectively a manual timeout, and will result in the request completing with status INTULocationStatusTimedOut.
-    If the requestID corresponds to a subscription, then the subscription will simply be canceled. */
+ For one-time location requests, this is effectively a manual timeout, and will result in the request completing with status INTULocationStatusTimedOut.
+ If the requestID corresponds to a subscription, then the subscription will simply be canceled. */
 - (void)forceCompleteLocationRequest:(INTULocationRequestID)requestID;
 
 /** Immediately cancels the location request (or subscription) with the given requestID (if it exists), without executing the original request block. */
@@ -135,9 +135,9 @@ __INTU_ASSUME_NONNULL_BEGIN
 /**
  Creates a subscription for heading updates that will execute the block once per update indefinitely (until canceled), assuming the heading update exceeded the heading filter threshold.
  If an error occurs, the block will execute with a status other than INTUHeadingStatusSuccess, and the subscription will be canceled automatically.
-
+ 
  @param block           The block to execute every time an updated heading is available. The status will be INTUHeadingStatusSuccess unless an error occurred.
-
+ 
  @return The heading request ID, which can be used to cancel the subscription of heading updates to this block.
  */
 - (INTUHeadingRequestID)subscribeToHeadingUpdatesWithBlock:(INTUHeadingRequestBlock)block;
@@ -145,10 +145,9 @@ __INTU_ASSUME_NONNULL_BEGIN
 /** Immediately cancels the heading subscription request with the given requestID (if it exists), without executing the original request block. */
 - (void)cancelHeadingRequest:(INTUHeadingRequestID)requestID;
 
-#pragma mark - Additions
 
-/** It is possible to force enable background location fetch even if your set any kind of Authorizations */
-- (void)setBackgroundLocationUpdate:(BOOL) enabled;
+/** Indicates that locationRequests array is empty therefore the location manager is idle */
+- (BOOL)isIdle;
 
 @end
 
